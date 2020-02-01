4dd4e95290f526162f51ba3693e5b41941167730
Nextsummer <sjing108@gmail.com>
2020-02-01 20:22:57 +0800

    异常处理

--- src/main/java/com/exam/common/advice/BasicExceptionAdvice.java
+++ src/main/java/com/exam/common/advice/BasicExceptionAdvice.java
@@ -1,5 +1,8 @@
 package com.exam.common.advice;
 
+import com.exam.common.exception.ExamException;
+import com.exam.common.exception.ExceptionResult;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,11 +10,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class BasicExceptionAdvice {
 
+    //捕获security异常
     @ExceptionHandler(RuntimeException.class)
     public String handleException(RuntimeException e){
         if (e instanceof AccessDeniedException){
             return "redirect:/403";
         }
         return "redirect:/500";
+    }
+
+    //统一异常处理
+    @ExceptionHandler(ExamException.class)
+    public ResponseEntity<ExceptionResult> handleLyException(ExamException e) {
+        return ResponseEntity.status(e.getStatus()).body(new ExceptionResult(e));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.status(500).body(e.getMessage());
     }
 }
